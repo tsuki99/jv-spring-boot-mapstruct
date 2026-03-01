7cf96445b3041168b41bbc3bf9158a64972d8739
Vlad Rublenko <vladik.rar@ukr.net>
2026-03-01 20:50:32 +0200

    initial commit

--- src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
+++ src/main/java/mate/academy/mapstruct/mapper/StudentMapper.java
@@ -1,14 +1,51 @@
 package mate.academy.mapstruct.mapper;
 
+import java.util.List;
+import mate.academy.mapstruct.config.MapperConfig;
 import mate.academy.mapstruct.dto.student.CreateStudentRequestDto;
 import mate.academy.mapstruct.dto.student.StudentDto;
 import mate.academy.mapstruct.dto.student.StudentWithoutSubjectsDto;
+import mate.academy.mapstruct.model.Group;
 import mate.academy.mapstruct.model.Student;
+import mate.academy.mapstruct.model.Subject;
+import org.mapstruct.AfterMapping;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.MappingTarget;
+import org.mapstruct.Named;
 
+@Mapper(config = MapperConfig.class, uses = {GroupMapper.class, SubjectMapper.class})
 public interface StudentMapper {
+    @Mapping(source = "group.id", target = "groupId")
+    @Mapping(target = "subjectIds", ignore = true)
     StudentDto toDto(Student student);
 
+    @AfterMapping
+    default void setSubjectIds(@MappingTarget StudentDto studentDto, Student student) {
+        List<Long> listIds = student.getSubjects().stream()
+                .map(Subject::getId)
+                .toList();
+        studentDto.setSubjectIds(listIds);
+    }
+
+    @Mapping(source = "group.id", target = "groupId")
     StudentWithoutSubjectsDto toStudentWithoutSubjectsDto(Student student);
 
+    @Mapping(source = "groupId", target = "group", qualifiedByName = "mapGroupIdToGroup")
+    @Mapping(target = "subjects", ignore = true)
     Student toModel(CreateStudentRequestDto requestDto);
+
+    @AfterMapping
+    default void setSubjects(@MappingTarget Student student,
+                             CreateStudentRequestDto studentRequestDto) {
+        List<Subject> subjectList = studentRequestDto.subjects().stream()
+                .map(Subject::new)
+                .toList();
+        student.setSubjects(subjectList);
+    }
+
+    @Named("mapGroupIdToGroup")
+    default Group mapGroupIdToGroup(Long id) {
+        return id == null ? null : new Group(id);
+    }
 }
